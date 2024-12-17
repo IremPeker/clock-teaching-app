f74343df2a7347790b2b3ed17c16636622cb0f94
Irem Peker <irem.peker@gmx.de>
2024-12-17 22:09:54 +0100

    Add types to test file

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -7,18 +7,19 @@ beforeEach(() => {
 
 describe("App component is rendered correctly", () => {
   test("Analog clock is rendered", () => {
-    const analogClock = screen.getByTestId("analog-clock");
+    const analogClock: HTMLElement = screen.getByTestId("analog-clock");
     expect(analogClock).toBeInTheDocument();
   });
   test("Input fields are rendered", () => {
-    const morningInput = screen.getByTestId("morning-input");
-    const eveningInput = screen.getByTestId("evening-input");
+    const morningInput: HTMLInputElement = screen.getByTestId("morning-input");
+    const eveningInput: HTMLInputElement = screen.getByTestId("evening-input");
     expect(morningInput).toBeInTheDocument();
     expect(eveningInput).toBeInTheDocument();
   });
   test("Buttons are rendered", () => {
-    const checkButton = screen.getByTestId("check-button");
-    const newTimeButton = screen.getByTestId("new-time-button");
+    const checkButton: HTMLButtonElement = screen.getByTestId("check-button");
+    const newTimeButton: HTMLButtonElement =
+      screen.getByTestId("new-time-button");
     expect(checkButton).toBeInTheDocument();
     expect(newTimeButton).toBeInTheDocument();
   });
